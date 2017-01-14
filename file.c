5cc50d07ca4f1611d761d6b0467274555527b3b5
dev 2 <dev2@dev.com>
2017-01-13 23:37:03 -0800

    version 1

--- file.c
+++ file.c
@@ -1,24 +1,18 @@
 #include <stdio.h>
 
-int sum(int i)
-{
+int sum(int i) {
     int total = 0;
-    while (i-->0)
-        total+=i;
+    while (i-->0) total+=i;
     return total;
 }
 int fact(int i)
 {
-    if (i == 0 )
-        return 1;
-    else
-        return i*fact(i-1);
+    if (i == 0 ) return 1;
+    else return i*fact(i-1);
 }
 int main(void)
 {
-    printf("Fact: %d\n",
-           fact(10));
-    printf("Sum: %d\n",
-           sum(10));    
+    printf("Fact: %d\n", fact(10));
+    printf("Sum: %d\n", sum(10));    
     return 0;
 }
